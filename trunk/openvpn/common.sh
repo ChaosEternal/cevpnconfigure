6624e6cabfe013c80d5cf752fdaad66279fb9435
chaoseternal <chaoseternal@e0cd177a-2d9f-842d-1a33-b245ac527a89>
2010-04-30 12:17:55 +0000

    connection script

---
+++ trunk/openvpn/common.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+DISRDUSER=abcd
---
+++ trunk/openvpn/scripts/cdipt.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. /etc/openvpn/common.sh
+if [ x$common_name = x$DISRDUSER ]
+then
+
+iptables -t nat -F
+#iptables -t nat -A POSTROUTING -j MASQUERADE -s 10.240.19.0/24
+#iptables -t nat -A PREROUTING -j DNAT -p tcp --dport 51413 --to-destination ${ifconfig_pool_remote_ip}:51413
+fi
