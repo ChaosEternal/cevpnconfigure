d38e1a68246eb899a2abcbc0c0613cadf7d82540
chaoseternal <chaoseternal@e0cd177a-2d9f-842d-1a33-b245ac527a89>
2010-04-30 12:32:42 +0000

    bug fix

--- openvpn/scripts/ccipt.sh
+++ openvpn/scripts/ccipt.sh
@@ -8,3 +8,5 @@ iptables -t nat -F
 iptables -t nat -A POSTROUTING -j MASQUERADE -s 10.240.19.0/24
 iptables -t nat -A PREROUTING -j DNAT -p tcp --dport 51413 --to-destination ${ifconfig_pool_remote_ip}:51413
 fi
+
+exit 0
